1554e083db1d01f76544698615f02bcce7f7793a
AriosJentu <darthpoezd@gmail.com>
2019-07-28 18:27:50 +1000

    Mix two types of comparing images

--- comparator.py
+++ comparator.py
@@ -1,32 +1,8 @@
-# from PIL import Image
+from PIL import Image
 import cv2
 import numpy as np
 
-def compare(img1, img2):
-
-	"""
-	i1 = Image.open(img1)
-	i2 = Image.open(img2)
-
-	if (i1.mode != i2.mode):
-		print("Different kinds of images.")
-
-	if (i1.size != i2.size):
-		print("Different sizes.")
- 
-	pairs = zip(i1.getdata(), i2.getdata())
-
-	if len(i1.getbands()) == 1:
-		# for gray-scale jpegs
-		dif = sum(abs(p1-p2) for p1,p2 in pairs)
-	else:
-		dif = sum(abs(c1-c2) for p1,p2 in pairs for c1,c2 in zip(p1,p2))
-	 
-	ncomponents = i1.size[0] * i1.size[1]
-	print(dif, ncomponents, dif/255, dif*100/(255*ncomponents))
-	percentage = (dif/255) * (100 / ncomponents)
-	#print("Difference (percentage):", percentage, ncomponents, dif)
-	"""
+def compare1(img1, img2):
 
 	aimg1 = cv2.imread(img1, 0)
 	aimg2 = cv2.imread(img2, 0)
@@ -36,3 +12,30 @@ def compare(img1, img2):
 	percentage = (np.count_nonzero(res) * 100)/ res.size
 	
 	return percentage
+
+def compare2(img1, img2):
+
+	i1 = Image.open(img1)
+	i2 = Image.open(img2)
+
+	if (i1.mode != i2.mode):
+		print("Different kinds of images.")
+
+	if (i1.size != i2.size):
+		print("Different sizes.")
+ 
+	pairs = zip(i1.getdata(), is2.getdata())
+
+	if len(i1.getbands()) == 1:
+		# for gray-scale jpegs
+		dif = sum(abs(p1-p2) for p1,p2 in pairs)
+	else:
+		dif = sum(abs(c1-c2) for p1,p2 in pairs for c1,c2 in zip(p1,p2))
+	 
+	ncomponents = i1.size[0] * i1.size[1]
+	percentage = (dif/255) * (100 / ncomponents)
+
+	return percentage
+
+def compare(img1, img2):
+	return max(compare1(img1, img2), compare2(img1, img2))
