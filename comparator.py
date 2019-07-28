f4fc5d24a29b79e5e4a9411c1d71d4d936b61dd5
AriosJentu <darthpoezd@gmail.com>
2019-07-28 18:17:44 +1000

    Add another way to compute difference between pictures

--- comparator.py
+++ comparator.py
@@ -1,7 +1,10 @@
-from PIL import Image
+# from PIL import Image
+import cv2
+import numpy as np
 
 def compare(img1, img2):
 
+	"""
 	i1 = Image.open(img1)
 	i2 = Image.open(img2)
 
@@ -19,8 +22,17 @@ def compare(img1, img2):
 	else:
 		dif = sum(abs(c1-c2) for p1,p2 in pairs for c1,c2 in zip(p1,p2))
 	 
-	ncomponents = i1.size[0] * i1.size[1] * 3
-	percentage = (dif/255) * (100/ncomponents)
+	ncomponents = i1.size[0] * i1.size[1]
+	print(dif, ncomponents, dif/255, dif*100/(255*ncomponents))
+	percentage = (dif/255) * (100 / ncomponents)
 	#print("Difference (percentage):", percentage, ncomponents, dif)
+	"""
+
+	aimg1 = cv2.imread(img1, 0)
+	aimg2 = cv2.imread(img2, 0)
+
+	#--- take the absolute difference of the images ---
+	res = cv2.absdiff(aimg1, aimg2)
+	percentage = (np.count_nonzero(res) * 100)/ res.size
 	
 	return percentage
